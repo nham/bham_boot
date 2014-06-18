0550dec1e6ae645bae2622bb83b04c76c6bbb1db
nham <hamann.nick@gmail.com>
2014-06-18 15:51:28 -0400

    Amend boot.asm to load kernel into memory via BIOS interrupts.

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
-all: kernel boot
+all: os-image
+	qemu-system-i386 os-image
+
+os-image: kernel boot
+	cat boot_sect.bin kernel.bin > os-image
 
 kernel: kernel.c
 	gcc -ffreestanding -c kernel.c -o kernel.o
 	ld  -o kernel.bin -Ttext 0x1000 kernel.o --oformat binary
 
 boot: boot.asm
-	nasm boot.asm
-	qemu-system-i386 boot
+	nasm boot.asm -o boot_sect.bin
--- boot.asm
+++ boot.asm
@@ -1,18 +1,38 @@
 [org 0x7c00]
+KERNEL_OFFSET equ 0x1000
 
-mov bp, 0x9000
-mov sp, bp
-mov bx, MSG_REAL_MODE
-call print_string
+    mov [BOOT_DRIVE], dl ; BIOS stores boot drive in DL
 
-call switch_to_pm
+    mov bp, 0x9000
+    mov sp, bp
 
-jmp $ ; jump to this line over and over
+    mov bx, MSG_REAL_MODE
+    call print_string
+
+    call load_kernel ; put kernel in memory
+
+    call switch_to_pm
+
+    jmp $ ; jump to this line over and over
 
 %include "print_string.asm"
+%include "disk_load.asm"
 %include "gdt.asm"
 %include "print_string_pm.asm"
 %include "switch_to_pm.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print_string
+
+    ; prep for disk_load
+    mov bx, KERNEL_OFFSET ; where in memory we are loading the kernel
+    mov dh, 1 ; number of sectors
+    mov dl, [BOOT_DRIVE] ; letter of the drive? TODO
+    call disk_load
+
+    ret
 
 [bits 32]
 
@@ -20,10 +40,14 @@ BEGIN_PM:
     mov ebx, MSG_PROT_MODE
     call print_string_pm
 
+    call KERNEL_OFFSET
+
     jmp $ ; jump to this line over and over
 
-MSG_REAL_MODE db "In real mode for now", 0
+BOOT_DRIVE db 0
+MSG_REAL_MODE db "Keeping it real.", 0
 MSG_PROT_MODE db "Made it to protected mode. lalalalala", 0
+MSG_LOAD_KERNEL db "Loading kernel nowwww...", 0
 
 ; bootsector padding + adding the boot signature
 times 510-($-$$) db 0
